56fa09b7566df4b357751bc76bfb37cd2d7b5406
Sheila Barron <sheilabarron@me.com>
2018-10-02 22:47:26 +0200

    aset commit

---
+++ src/ASet.java
@@ -0,0 +1,25 @@
+
+public class ASet implements ASetInterface {
+	
+	public Vertex[] vertexes;
+	
+	public ASet(){
+		this.vertexes = null;
+	}
+	
+
+	@Override
+	public void setASet(Vertex[] vertexes) {
+		// TODO Auto-generated method stub
+		this.vertexes = vertexes;
+		return;
+		
+	}
+
+	@Override
+	public ASet getASet() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
