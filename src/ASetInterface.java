9341c4d787b47dd040d1d8ea49baa91b68236908
Sheila Barron <sheilabarron@me.com>
2018-10-02 22:39:34 +0200

    gitignore

---
+++ src/ASetInterface.java
@@ -0,0 +1,10 @@
+public interface ASetInterface {
+	
+	// Interface has no constructor
+	
+	// To set the values of the vertexes in the set
+	public void setASet(Vertex[]vertexes);
+	// To get the set
+	public ASet getASet();
+
+}
