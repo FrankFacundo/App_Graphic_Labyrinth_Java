c085702dcef80f1c3076a4c686b360b295a01da3
Sheila Barron <sheilabarron@me.com>
2018-10-02 22:48:10 +0200

    vertex init

---
+++ src/Vertex.java
@@ -0,0 +1,20 @@
+public class Vertex implements VertexInterface {
+
+	public boolean differentFrom(Vertex otherVertex) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isInASet(ASet set) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public int getDistance() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
