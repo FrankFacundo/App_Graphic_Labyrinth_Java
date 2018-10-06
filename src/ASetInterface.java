c3675590abbba0f57d3b37c174ec76a4f196a9d4
Sheila Barron Huerta <sheila.barronhuerta@telecom-paristech.fr>
2018-10-07 01:52:14 +0200

    Update ASetInterface.java

--- src/ASetInterface.java
+++ src/ASetInterface.java
@@ -1,10 +1,22 @@
 public interface ASetInterface {
-	
-	// Interface has no constructor
+    
+    // To add a vertex to the set
+	public void add(VertexInterface vertex);
 	
 	// To set the values of the vertexes in the set
-	public void setASet(Vertex[]vertexes);
-	// To get the set
-	public ASet getASet();
+	public void setASet(VertexInterface[]vertexes);
+	
+	// To get the set values (vertexes)
+	public VertexInterface[] getASetVertex();
+	
+	// To get the number of vertexes in the set
+    public int getLengthASet();
+	
+	// To verify if a vertex is in the set
+    public boolean isVertexInASet(VertexInterface vertex);
+
+    // To get the index of a vertex in the set, -1 if the vertex is not in the set
+    public int getIndexOfVertex(VertexInterface vertex);
+
 
 }
