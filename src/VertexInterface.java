aecbae5196340347440aa4db22e3fe4b16090e0f
Sheila Barron Huerta <sheila.barronhuerta@telecom-paristech.fr>
2018-10-07 17:24:04 +0200

    Update VertexInterface.java

--- src/VertexInterface.java
+++ src/VertexInterface.java
@@ -1,7 +1,16 @@
-
 public interface VertexInterface {
+    
+    // To get the index of current vertex 
+         public int getIndex();
+    
+    // To get the distance to another vertex (weight)
+    	public int getDistance(VertexInterface vertex);
+    	
+	// To add an adjacent vertex 
+        public void addAdjacentVertex(VertexInterface vertex);
+        
+    // To get all adjacent vertexes
+        public ArrayList<VertexInterface> getAdjacentVertexes(); 
 
-	public boolean differentFrom(Vertex otherVertex);
-	public int getDistance();
-	boolean isInASet(ASet set);
+
 }
