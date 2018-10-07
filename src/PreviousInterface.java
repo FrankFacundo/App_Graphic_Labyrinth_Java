299d1e002ec5caf3108a1f78f3850c9a730021d4
Sheila Barron Huerta <sheila.barronhuerta@telecom-paristech.fr>
2018-10-07 17:45:52 +0200

    Update PreviousInterface.java

--- src/PreviousInterface.java
+++ src/PreviousInterface.java
@@ -1,4 +1,10 @@
-
 public interface PreviousInterface {
+    
+    // To get the previous vertex 
+        public VertexInterface getPreviousVertex(ASetInterface aset);
+        
+    // To set the previous vertex 
+        public void setPreviousVertex(ASetInterface aset, VertexInterface vertex);
+
 
 }
