14f429525315e1e17d40324252275e9ee3288c66
Sheila Barron <sheilabarron@me.com>
2018-10-16 01:21:55 +0200

    Vertex updates

--- src/Vertex.java
+++ src/Vertex.java
@@ -2,37 +2,122 @@ import java.util.ArrayList;
 
 public class Vertex implements VertexInterface {
 
-	@Override
-	public int getValue() {
-		// TODO Auto-generated method stub
-		return 0;
+	/**
+	 * The value of the vertex, shall not be modified
+	 */
+	private final String value; 
+	/**
+	 * The list of vertexes that are adjacent to the current vertex
+	 */
+	private ArrayList<VertexInterface> adjacents;
+	/**
+	 * The list of distances between the current vertex and the adjacent ones
+	 * Same indexes as in adjacents list
+	 */
+	private ArrayList<Integer> distanceToAdjacents;
+
+	/**
+	 * Vertex constructor
+	 * @param value, the String value contained in the vertex
+	 */
+	public Vertex(String value){
+
+		this.value = value;
+		this.adjacents = new ArrayList<VertexInterface>();
+		this.distanceToAdjacents = new ArrayList<Integer>();
 	}
-	
+
+
+	/**
+	 * To get the String value contained in the vertex
+	 * @return the String associated to the vertex
+	 */
+	public String getValue() {
+
+		return this.value;
+	}
+
+	/**
+	 * We consider two vertex equal if they contain the same String value 
+	 * Adjacent vertexes are not taken into account for the comparison
+	 */
+	@Override
+	public boolean equals(Object o) {
+		// If the object is compared with itself then return true   
+		if (o == this) { 
+			return true; 
+		} 
+
+		// Check if o is an instance of Vertex
+		//  "null instanceof [type]" also returns false
+		if (!(o instanceof Vertex)) { 
+			return false; 
+		} 
+
+		// Typecast o to Vertex so that we can compare data   
+		Vertex v = (Vertex) o; 
+
+		// Compare data and return accordingly 
+
+		if (this.value == null && v.value != null) {
+			return false;
+
+		} else if (!this.value.equals(v.value)){
+			return false;
+
+		}
+		// If we have not entered to the other conditions
+		return true; 
+
+	}
 
 	@Override
+	public String toString(){
+
+		return "[" + this.value + "]";
+
+	}
+
+
+	
+	/* (non-Javadoc)
+	 * @see VertexInterface#getDistanceTo(VertexInterface)
+	 */
 	public int getDistanceTo(VertexInterface anotherVertex) {
-		// TODO Auto-generated method stub
-		return 0;
+		
+		if (this.adjacents.contains(anotherVertex)){
+		  int index = this.adjacents.indexOf(anotherVertex);
+		  return this.distanceToAdjacents.get(index);
+		  
+		} else {
+			return -1;
+		}
+		
 	}
+
 	
-	@Override
-	public void addAdjacentVertex(VertexInterface vertex) {
-		// TODO Auto-generated method stub
+	public void addAdjacentVertex(VertexInterface vertex, int distance) {
 		
+		this.adjacents.add(vertex);
+		this.distanceToAdjacents.add(distance);
+
 	}
 
-	@Override
 	public ArrayList<VertexInterface> getAdjacentVertexes() {
-		// TODO Auto-generated method stub
-		return null;
+
+		return this.adjacents;
 	}
 
 
-	@Override
 	public void setDistanceTo(VertexInterface anotherVertex, int distance) {
-		// TODO Auto-generated method stub
-		
+
+		if (this.adjacents.contains(anotherVertex)){
+			  int index = this.adjacents.indexOf(anotherVertex);
+			  this.distanceToAdjacents.set(index, distance);		  
+		} 
+
 	}
+
 
 
 }
--- src/VertexInterface.java
+++ src/VertexInterface.java
@@ -2,19 +2,47 @@ import java.util.ArrayList;
 
 public interface VertexInterface {
 
-	// To get the value of current vertex 
-	public int getValue();
 
-	// To get the distance to another vertex (weight)
+	/**
+	 * To get the String value contained in the vertex
+	 * @return the String associated to the vertex
+	 */
+	public String getValue();
+	
+	/**
+	 * 
+	 * @return the String representation of the vertex
+	 */
+	public String toString();
+	
+	/**
+	 * 
+	 * @return true if two vertexes are equal, else false
+	 */
+	public boolean equals(Object o);
+	
+
+	/**
+	 * To get the distance to another vertex (weight)
+	 * @param anotherVertex, the other vertex
+	 * @return the distance between the current and the other vertex, -1 if anotherVertex is not adjacent to the current vertex
+	 */
 	public int getDistanceTo(VertexInterface anotherVertex);
 
 	// To set the distance to a vertex (?)
 	public void setDistanceTo(VertexInterface anotherVertex, int distance);
 
-	// To add an adjacent vertex 
-	public void addAdjacentVertex(VertexInterface vertex);
+	/**
+	 * To add an adjacent vertex
+	 * @param vertex, the vertex to add
+	 * @param distance, the distance between vertex and the current vertex
+	 */ 
+	public void addAdjacentVertex(VertexInterface vertex, int distance);
 
-	// To get all adjacent vertexes
+	/**
+	 * To get all adjacent vertexes
+	 * @return a list containing all adjacent vertexes
+	 */
 	public ArrayList<VertexInterface> getAdjacentVertexes(); 
 
 
