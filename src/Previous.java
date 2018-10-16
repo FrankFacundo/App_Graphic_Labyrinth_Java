22746ad6819068fcf1cb224ce70ff408b202d789
Sheila Barron <sheilabarron@me.com>
2018-10-17 00:12:42 +0200

    Previous updates

--- src/Previous.java
+++ src/Previous.java
@@ -1,16 +1,55 @@
+import java.util.ArrayList;
 
 public class Previous implements PreviousInterface{
 
-	@Override
-	public VertexInterface getPreviousVertex(ASetInterface aset) {
-		// TODO Auto-generated method stub
-		return null;
+	// We could use hashmap (associative array instead of two lists)
+	
+	/**
+	 * This list will contain the vertexes 
+	 */
+	private ArrayList<VertexInterface> vertexes;
+	/**
+	 * This list will contain the previous vertexes (associated with the vertexes in the vertexes list)
+	 * Same indexes as in pathList list
+	 */
+	private ArrayList<VertexInterface> previousVertexes;
+
+	/**
+	 * The constructor initializes both the vertex and the previous vertex list
+	 */
+	public Previous(){
+
+		this.vertexes = new ArrayList<VertexInterface>();
+		this.previousVertexes = new ArrayList<VertexInterface>();
+
 	}
 
-	@Override
-	public void setPreviousVertex(VertexInterface vertex) {
-		// TODO Auto-generated method stub
-		
+	public VertexInterface getPreviousVertex(VertexInterface vertex) {
+
+		if (this.vertexes.contains(vertex)){
+			int index = this.vertexes.indexOf(vertex);
+			return this.previousVertexes.get(index);
+
+		} else {
+			return null;
+		}
+
 	}
+
+	public void setPreviousVertex(VertexInterface vertex, VertexInterface previousVertex) {
+		if (this.vertexes.contains(vertex)){
+			
+			int index = this.vertexes.indexOf(vertex);
+			this.previousVertexes.set(index, previousVertex);
+
+		} else {
+			this.vertexes.add(vertex);
+			this.previousVertexes.add(previousVertex);
+
+		}
+
+	}
+
+	// By going through the previous vertexes we will find the shortest path
 
 }
