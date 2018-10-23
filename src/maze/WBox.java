278bdbe750b36232f87665386134cc24a5a4fa09
Sheila BARRON HUERTA <sheila.barronhuerta@telecom-paristech.fr>
2018-10-23 11:53:10 +0200

    wbox correction

--- src/maze/WBox.java
+++ src/maze/WBox.java
@@ -1,6 +1,11 @@
 package maze;
 
-public class WBox extends Mbox
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import dijsktra.VertexInterface;
+
+public class WBox extends MBox
 {
 	public WBox(int column, int row)
 	{
@@ -12,8 +17,32 @@ public class WBox extends Mbox
 		
 	}
 	
-	public void getLabel()
-	{
+
+	public String getLabel() {
+		return super.getLabel();
+	}
+
+	@Override
+	public int getDistanceTo(VertexInterface anotherVertex) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void addAdjacentVertex(VertexInterface vertex, int distance) {
+		// TODO Auto-generated method stub
 		
+	}
+
+	@Override
+	public ArrayList<VertexInterface> getAdjacentVertexes() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public HashMap<VertexInterface, Integer> getAdjacentVertexesAndDistances() {
+		// TODO Auto-generated method stub
+		return null;
 	}
 }
