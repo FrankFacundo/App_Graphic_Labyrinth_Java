86b85399e4869ba64982108bba365fed5036e13a
Sheila Barron Huerta <sheila.barronhuerta@telecom-paristech.fr>
2018-11-05 07:29:37 +0100

    Update MBox.java

--- src/maze/MBox.java
+++ src/maze/MBox.java
@@ -4,15 +4,17 @@ import dijsktra.VertexInterface;
 
 public abstract class MBox implements VertexInterface{
 	
+	private Maze maze;
 	private int column; 
 	private int row;
 	private String label;
 	
-	public MBox(int column, int row, String label){
-		
+	public MBox(Maze maze, int column, int row, String label){
+	
+		this.maze = maze;
 		this.column = column;
 		this.row = row;
-		this.label = label;
+		this.label = "(" + this.row + "," + this.column + ")";
 		
 	}
 
