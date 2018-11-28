84f91ab46e5d4fb185080bf3f23dd191c147f924
Frank <frank.f96@hotmail.com>
2018-11-28 11:51:14 +0100

    Color

---
+++ src/ui/ColorIndicator.java
@@ -0,0 +1,12 @@
+package ui;
+import javax.swing.*;
+public final class ColorIndicator extends JPanel
+{
+  private DrawingApp drawingApp;
+
+  public ColorIndicator (DrawingApp drawingApp)
+  {
+    super();
+    this.drawingApp = drawingApp;
+  }
+}
