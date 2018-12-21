3b3ae8206ca0574f26169e9c70a43c40f3604472
Sheila Barron <sheilabarron@me.com>
2018-12-21 12:22:19 +0100

    Begin of Model implementation

---
+++ src/model/Model.java
@@ -0,0 +1,32 @@
+package model;
+
+import java.awt.Graphics;
+import java.util.ArrayList;
+import java.util.Observable;
+
+import javax.swing.*;
+
+public class Model extends Observable {
+
+
+	private final ArrayList<Case> editedCases  = new ArrayList<Case>(10) ;
+	private Case selectedCase = new Case();
+
+
+	public Case getSelectedCase() {
+		return selectedCase;
+	}
+
+	public final void paintCases(Graphics g) {
+		for (Case c : editedCases)
+			c.paint(g,false,false) ;
+
+		if (selectedCase != null)
+			selectedCase.paint(g,true,false) ;
+
+		if (currentCase != null)
+			currentCase.paint(g,false,true) ;
+	}	
+
+
+}
