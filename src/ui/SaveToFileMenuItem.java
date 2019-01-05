5ccb4ec5dce5271752d7168da255726800cbc086
Frank <frank.f96@hotmail.com>
2019-01-05 14:31:15 +0100

    Revert "Revert "Debug getShortestPath""
    This reverts commit 245174fcca99392dcd1c4265bdaebdfb17f98cea.

--- src/ui/SaveToFileMenuItem.java
+++ src/ui/SaveToFileMenuItem.java
@@ -1,7 +1,13 @@
 package ui;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.JMenuItem;
-public final class SaveToFileMenuItem extends JMenuItem
+import javax.swing.JOptionPane;
+
+import model.Model;
+public final class SaveToFileMenuItem extends JMenuItem implements ActionListener
 {
   private final MazeApp MazeApp;
 
@@ -9,6 +15,13 @@ public final class SaveToFileMenuItem extends JMenuItem
   {
     super("Save to file");
     this.MazeApp = MazeApp;
+    addActionListener(this) ;
   }
+  
+	public void actionPerformed(ActionEvent evt){
+	
+	
+	
+	}
   
 }
