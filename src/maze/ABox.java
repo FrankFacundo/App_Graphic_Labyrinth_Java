0da162273818b5bbbd007f6a74a651dfdc245ef5
Sheila Barron <sheilabarron@me.com>
2019-01-17 20:25:49 +0100

    commenting box classes

--- src/maze/ABox.java
+++ src/maze/ABox.java
@@ -1,45 +1,64 @@
 package maze;
 
+/**
+ *	ABox represents the arrival box (the arrival point in the maze) 
+ * 
+ */
+
 public final class ABox extends MBox {
 
+	/**
+	 * The constructor
+	 * @param maze, the maze to which the arrival box is linked
+	 * @param column, the column at which the arrival box will be placed
+	 * @param row, the row at which the arrival box will be placed
+	 * 
+	 */
 	public ABox(Maze maze, int column, int row) {
+
 		super(maze , column, row);
-	}
-	public void setLabel() {
 
 	}
 
+	@Override
 	public boolean isEmpty() {
-		return false;
-	}
 
+		return false;
+
+	}
 
 	@Override
 	public char getChar() {
 
 		return 'A';
+
 	}
 
 	@Override
 	public String getLabel() {
 
 		return "ARRIVAL";
+
 	}
+
 	@Override
 	public boolean isWall() {
 
 		return false;
+
 	}
+
 	@Override
 	public boolean isDepart() {
 
 		return false;
 	}
+
 	@Override
 	public boolean isArrival() {
 
 		return true;
-	}
 
+	}
 
 }
